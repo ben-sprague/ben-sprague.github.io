2b9519b38ae3a7f93e2df61d332d529c82590320
Ben Sprague <btsprague@gmail.com>
2025-07-14 22:08:59 -0400

    Add homepage images

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Ben Sprague',
-  tagline: 'Naval Officer, Engineer, Scholar',
+  tagline: 'US Naval Academy Midshipman',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,33 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Future Naval Officer',
+    Svg: require('@site/static/img/carlVinzon.jpg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Upon graduation from the US Naval Academy in May, 2026, I will comission as an
+        active duty submarine officer in the US Navy.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'Engineer',
+    Svg: require('@site/static/img/capstoneDay2025.jpg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        I am currently studying Aerospace Engineering, but I have always held an affinity for 
+        the ocean and hope to study Physical Oceanography in graduate school.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Scholar',
+    Svg: require('@site/static/img/waldenPond.jpg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        To be a successful scientest, engineer, and officer, I belive it is imparative to be
+        well read and a skilled writer. I read a wide variety of works, but I particularly
+        enjoy scientific memiors and transcendentalism works.
       </>
     ),
   },
